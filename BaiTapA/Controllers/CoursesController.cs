ca312cd520580ad0980fcec9b99e93047aa09ca1
quanghuy2601 <quanghuy7927@gmail.com>
2023-05-19 11:02:07 +0700

    Add project files.

---
+++ BaiTapA/Controllers/CoursesController.cs
@@ -0,0 +1,33 @@
+﻿using BaiTapA.Models;
+using BaiTapA.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTapA.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+
+        
+    }
+}
---
+++ BaiTapA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BaiTapA.Startup))]
+namespace BaiTapA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
